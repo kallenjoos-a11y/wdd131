92f74a295c29e41e286de0b8fdd9b1a80ea26161
Kallen Joos <kallen.joos@gmail.com>
2026-05-04 16:38:25 -0600

    first commit

---
+++ mission/index.js
@@ -0,0 +1,27 @@
+
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    let border = document.getElementById('content');
+    let img = document.querySelector('img');
+    let underline = document.querySelector('h3');
+
+    if (current == 'dark') {
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+        border.style.borderColor = "white";
+        underline.style.borderBottom = "1px solid white";
+        img.src = "byui-logo-white.png";
+    } else {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+        border.style.borderColor = "black";
+        underline.style.borderBottom = "1px solid black";
+        img.src = "https://wddbyui.github.io/wdd131/images/byui-logo-blue.webp";
+    }
+}           
+                    
