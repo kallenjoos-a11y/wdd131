3fd7a5016b9d7200981a54b6bab44f705ea5dff0
Kallen Joos <kallen.joos@gmail.com>
2026-09-24 11:02:15 -0600

    could be fun tho

---
+++ ponder/dom-theme.js
@@ -0,0 +1,35 @@
+// 1. Functions
+이름();
+function 이름(여자친구 = "undefined"){
+    console.log(`Hello functions! Your girlfriend is ${여자친구}`);
+}
+
+// invoke the sub routine
+// 2. Event Listeners
+
+// 3. If statements
+
+
+let selectElem = document.querySelector('#theme-select');
+let pageContent = document.querySelector('body');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current === 'ocean') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/ocean.jpg')";
+        pageContent.style.fontFamily = "Papyrus, fantasy";
+    } else if (current === 'forest') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/forest.jpg')";
+        pageContent.style.fontFamily = "Impact, sans-serif";
+    } else if (current === 'desert') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/desert.jpg')";
+        pageContent.style.fontFamily = "'Big Caslon', serif";
+    } else {
+        // default
+        document.body.style.backgroundImage = "none";
+        pageContent.style.fontFamily = "Georgia, serif";
+    }
+}
+          
